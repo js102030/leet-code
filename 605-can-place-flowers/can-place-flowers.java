a907f758835ce30427837e5b5866e2bda8cb788f
js102030 <121748946+js102030@users.noreply.github.com>
2024-01-29 10:55:28 +0900

    Time: 1 ms (97.74%) | Memory: 45.9 MB (9.19%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.java
@@ -0,0 +1,91 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (i == 0) {
+                if (flowerbed.length != 1) {
+                    if (flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                        ++count;
+                        flowerbed[i] = 1;
+                    }
+                } else {
+                    if (flowerbed[i] == 0) {
+                        ++count;
+                    }
+                }
+            } else if (i == flowerbed.length - 1) {
+                if (flowerbed[i - 1] == 0 && flowerbed[i] == 0) {
+                    ++count;
+                    flowerbed[i] = 1;
+                }
+            } else {
+                if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                    ++count;
+                    flowerbed[i] = 1;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return n <= count;
+    }
+}
